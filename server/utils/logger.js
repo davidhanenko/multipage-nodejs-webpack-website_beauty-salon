4bfc48dc53bdbdf36bbc3fe2013fc14992057d7d
davidhanenko <d.hanenko24@gmail.com>
2021-03-11 23:23:25 -0500

    small fix

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,17 +1,20 @@
 const { createLogger, format, transports } = require('winston')
 require('winston-mongodb')
+const mongoose = require('mongoose')
+
+const mongoConn = mongoose.connection
 
 const logger = createLogger({
   level: 'info',
   format: format.combine(format.timestamp(), format.colorize(), format.json()),
   transports: [
-      new transports.File({ filename: 'error.log', level: 'error' }),
-    // new transports.MongoDB({
-    //   db: process.env.DB_URL,
-    //   tryReconnect: true,
-    //   level: 'error',
-    //   collection: 'error-logs'
-    // }),
+    // new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.MongoDB({
+      db: mongoConn,
+      tryReconnect: true,
+      level: 'error',
+      collection: 'error-logs'
+    }),
     new transports.File({ filename: 'combined.log' })
   ]
 })
