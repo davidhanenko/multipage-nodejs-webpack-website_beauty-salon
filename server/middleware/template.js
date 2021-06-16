429d9a4ecb431cbfccbcb6227b9a92d4ed0c0a13
davidhanenko <d.hanenko24@gmail.com>
2021-06-15 23:07:00 -0400

    comment updates

--- server/middleware/template.js
+++ server/middleware/template.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 const ejs = require('ejs')
 const isDev = process.env.NODE_ENV === 'development'
 
+
 function getTemplateString(filename) {
   return new Promise((resolve, reject) => {
     axios
@@ -13,15 +14,22 @@ function getTemplateString(filename) {
   })
 }
 
+// custom function to render pages instead of Express method "render"
+// this will allow us to use .ejs files with Webpack hot reload in Dev mode
 async function renderEJS(res, filename, data) {
   const ext = '.ejs'
+  // remove extension .ejs from full fail name, and get all path to it(with nested folders)
   filename = filename.indexOf(ext) > -1 ? filename.split(ext)[0] : filename
   try {
     if (isDev) {
+      // template for current filename
       const template = await getTemplateString(`${filename}.ejs`)
+      // render file with Express render method
       let html = ejs.render(template, data)
+      // send rendered .ejs file like .html 
       res.send(html)
     } else {
+      // use Express render method to render file from views directory in Production mode
       res.render(filename, data)
     }
     return Promise.resolve()
