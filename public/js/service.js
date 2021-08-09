02ed94c40ac70dac7a2865e6962c2e1e24a91a47
davidhanenko <d.hanenko24@gmail.com>
2021-08-08 22:51:11 -0400

    skin tags removal, services buttons styles

--- public/js/service.js
+++ public/js/service.js
@@ -19,3 +19,23 @@ document.onreadystatechange = function () {
     document.querySelector('body').style.visibility = 'visible'
   }
 }
+
+//price buttons
+$('.pricesButtons').on('click', '.priceBtn', function () {
+  var i = $('.priceBtn').index(this)
+  var arr = $('.priceBtn')
+
+  if (i < $(arr).length / 2) {
+    // eslint-disable-next-line no-self-assign
+    i = i
+  } else {
+    i = i - $(arr).length / 2
+  }
+
+  var table = $('.pricesTable')[i]
+
+  $('.priceBtn').removeClass('priceBtnActive')
+  $('.pricesTable').removeClass('prices-active')
+  $(table).addClass('prices-active')
+  $(this).addClass('priceBtnActive')
+})
