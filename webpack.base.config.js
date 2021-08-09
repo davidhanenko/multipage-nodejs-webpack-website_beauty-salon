2a16d592035b0df50e254f27185c34f82ad596a0
davidhanenko <d.hanenko24@gmail.com>
2021-08-08 22:06:07 -0400

    skin tags removal

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -225,6 +225,12 @@ module.exports = {
       chunks: isDev ? ['service'] : ['service', 'manifest', 'vendors']
     }),
     new HtmlWebPackPlugin({
+      template:
+        '!!ejs-webpack-loader!./public/views/our_services/skin-tags-removal.ejs',
+      filename: 'views/our_services/skin-tags-removal.ejs',
+      chunks: isDev ? ['service'] : ['service', 'manifest', 'vendors']
+    }),
+    new HtmlWebPackPlugin({
       template: '!!ejs-webpack-loader!./public/views/gallery.ejs',
       filename: 'views/gallery.ejs',
       chunks: isDev ? ['gallery'] : ['gallery', 'manifest', 'vendors']
@@ -328,6 +334,14 @@ module.exports = {
       template:
         '!!ejs-webpack-loader!./public/views/admin/our_services/jet-peel.ejs',
       filename: 'views/admin/our_services/jet-peel.ejs',
+      chunks: isDev
+        ? ['admin_service']
+        : ['admin_service', 'manifest', 'vendors']
+    }),
+    new HtmlWebPackPlugin({
+      template:
+        '!!ejs-webpack-loader!./public/views/admin/our_services/skin-tags-removal.ejs',
+      filename: 'views/admin/our_services/skin-tags-removal.ejs',
       chunks: isDev
         ? ['admin_service']
         : ['admin_service', 'manifest', 'vendors']
