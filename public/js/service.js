24e199a9ec16efca4871e9786bedf3196cfb64ca
davidhanenko <d.hanenko24@gmail.com>
2021-08-09 23:53:06 -0400

    services buttont dropdown

--- public/js/service.js
+++ public/js/service.js
@@ -20,22 +20,26 @@ document.onreadystatechange = function () {
   }
 }
 
-//price buttons
-$('.pricesButtons').on('click', '.priceBtn', function () {
-  var i = $('.priceBtn').index(this)
-  var arr = $('.priceBtn')
+//services dropdown buttons
+$('.dropdown-services-buttons').on(
+  'click',
+  '.dropdown-service-button',
+  function () {
+    var i = $('.dropdown-service-button').index(this)
+    var arr = $('.dropdown-service-button')
 
-  if (i < $(arr).length / 2) {
-    // eslint-disable-next-line no-self-assign
-    i = i
-  } else {
-    i = i - $(arr).length / 2
+    if (i < $(arr).length / 2) {
+      // eslint-disable-next-line no-self-assign
+      i = i
+    } else {
+      i = i - $(arr).length / 2
+    }
+
+    // var table = $('.pricesTable')[i]
+
+    $('.dropdown-service-button').removeClass('dropdown-service-btn-active')
+    // $('.pricesTable').removeClass('prices-active')
+    // $(table).addClass('prices-active')
+    $(this).addClass('dropdown-service-btn-active')
   }
-
-  var table = $('.pricesTable')[i]
-
-  $('.priceBtn').removeClass('priceBtnActive')
-  $('.pricesTable').removeClass('prices-active')
-  $(table).addClass('prices-active')
-  $(this).addClass('priceBtnActive')
-})
+)
